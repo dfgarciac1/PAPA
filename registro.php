9d69434c552e7cd86cd33370f3b1da3f106a30f3
Dcg <53051443+dfgarciac1@users.noreply.github.com>
2020-08-17 14:15:51 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,12 +1,29 @@
 ?php 
+conexion();
+function conexion(){
+$host="127.0.0.1";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+
+}
+
+ 
 $Nombre=$_GET['nombre'];
 $Edad=$_GET['Edad'];
 $Correo=$_GET['Correo'];
-$Contrasena=$_GET['Contrasena'];
+$Contraseña=$_GET['Contraseña'];
 
 echo "<h2>Informacion Recibida </h2>";
-echo "El nombre del usuario es " . $Nombre."<br/>";
-echo "La Edad del usuario es " . $Edad."<br/>";
-echo "El Correo del usuario es " . $Correo."<br/>";
-echo "La Contraseña del usuario es " . $Contraseña."<br/>";
+echo "El nombre del usuario es ".$Nombre."<br/>";
+echo "La Edad del usuario es ".$Edad."<br/>";
+echo "El Correo del usuario es ".$Correo."<br/>";
+echo "La Contraseña del usuario es ".$Contraseña."<br/>";
 ?>  
