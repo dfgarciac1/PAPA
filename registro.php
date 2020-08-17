81c1d82ff0aefce290c82056c32b451039a714e6
Dcg <53051443+dfgarciac1@users.noreply.github.com>
2020-08-17 12:13:32 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,1 +1,12 @@
-?php echo "Hola, mundo\n\n";
+?php 
+$Nombre=$_GET['nombre'];
+$Edad=$_GET['Edad'];
+$Correo=$_GET['Correo'];
+$Contraseña=$_GET['Contraseña'];
+
+echo "<h2>Informacion Recibida </h2>";
+echo "El nombre del usuario es " . $Nombre."<br/>";
+echo "La Edad del usuario es " . $Edad."<br/>";
+echo "El Correo del usuario es " . $Correo."<br/>";
+echo "La Contraseña del usuario es " . $Contraseña."<br/>";
+?>  
